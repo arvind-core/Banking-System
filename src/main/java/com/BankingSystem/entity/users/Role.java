fbf52ea0ade2cb513d6ac73569ce735d879b40ab
Akash Dhakar <akash_yrr@icloud.com>
2026-04-11 23:52:28 +0530

    Added comments in Role ENum class.

--- src/main/java/com/BankingSystem/entity/users/Role.java
+++ src/main/java/com/BankingSystem/entity/users/Role.java
@@ -1,6 +1,8 @@
 package com.BankingSystem.entity.users;
 
 public enum Role {
+    // This Enum will hold the roles a user can have, this is not hard coded because we can make mistakes when hardcoding it everytime.
+    // If we use Enum to hold roles we just refer to the Enum, We don't actaul hard code the values so we make minimal error.
     CUSTOMER,
     BRANCH_MANAGER,
     ADMIN
